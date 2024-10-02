3d9c3a636a462c3c92c246bdfcc61533460da1d7
mrzhang <miaoranzhang@gmail.com>
2024-10-02 13:25:28 +0200

    add missing tempate to eval_zero.sh

--- experiments/llm/eval_zero.sh
+++ experiments/llm/eval_zero.sh
@@ -5,11 +5,12 @@ source $PROJECT_DIR/scripts/setup.sh
 
 task_list=mafand_e2t,mafand_t2e
 n_shot=0
+template=completion_template
 retriever=zero
 data_seed=0
 bs=16
 
 for model in bloomz xglm mt0 llama2; do
     output_dir=$PROJECT_DIR/results/${model}/${n_shot}_shot/
-    bash $PROJECT_DIR/scripts/eval_llm.sh $model $task_list $n_shot $retriever $data_seed $output_dir $bs
+    bash $PROJECT_DIR/scripts/eval_llm.sh $model $task_list $template $n_shot $retriever $data_seed $output_dir $bs
 done
